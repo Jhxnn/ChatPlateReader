2df1530527897d9f2e92e78a0d7c764bd2a088b6
Hilixx <jhonalizan@gmail.com>
2025-03-01 10:22:14 -0300

    userService

--- src/main/java/com/ChatPlateReader/controllers/UserController.java
+++ src/main/java/com/ChatPlateReader/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.ChatPlateReader.controllers;
 
+
 import java.util.List;
 import java.util.UUID;
 
@@ -10,37 +11,74 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ChatPlateReader.dtos.AuthDto;
 import com.ChatPlateReader.dtos.UserRequestDto;
 import com.ChatPlateReader.dtos.UserResponseDto;
-import com.ChatPlateReader.models.User;
+import com.ChatPlateReader.infra.security.TokenService;
+import com.ChatPlateReader.repositories.UserRepository;
+import com.ChatPlateReader.services.EmailService;
 import com.ChatPlateReader.services.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    UserService userService;
+	@Autowired
+	UserService userService;
+	
+	@Autowired
+	EmailService emailService;	
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	TokenService tokenService;
+	
+	
+	@Operation(description = "Lista todos os usuarios")
+	@GetMapping
+	public ResponseEntity<List<UserResponseDto>> findAll(){
+		return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+	}
+	
+	@Operation(description = "Lista usuario pelo id")
+	@GetMapping("/{id}")
+	public ResponseEntity<UserResponseDto> findById(@PathVariable(name = "id")UUID id){
+		return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
+	}
+	
+	@Operation(description = "Logar usuario")
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@RequestBody @Valid AuthDto authDto) {
+		return ResponseEntity.ok().body(userService.returnToken(authDto));
+	}
+	
+	@Operation(description = "Registrar usuario")
+	@PostMapping("/register")
+	public ResponseEntity<UserResponseDto> register(@RequestBody @Valid  UserRequestDto userRequestDto) {
+		return ResponseEntity.ok().body(userService.createUser(userRequestDto));
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserResponseDto> findById(@PathVariable(name = "id") UUID id) {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<UserResponseDto>> findAll() {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
-    }
-
-   
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable(name = "id") UUID id) {
-        userService.deleteUser(id);
-        return ResponseEntity.noContent().build();
-    }
+	@Operation(description = "Atualizar usuario")
+	@PutMapping("/{id}")
+	public ResponseEntity<UserResponseDto> updateUser(@PathVariable(name = "id")UUID id,
+			@RequestBody UserRequestDto userRequestDto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.updateUser(id, userRequestDto));
+	}
+	
+	@Operation(description = "Deletar usuario")
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable(name = "id")UUID id){
+		userService.deleteUser(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/ChatPlateReader/services/UserService.java
+++ src/main/java/com/ChatPlateReader/services/UserService.java
@@ -5,10 +5,16 @@ import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.ChatPlateReader.dtos.AuthDto;
 import com.ChatPlateReader.dtos.UserRequestDto;
 import com.ChatPlateReader.dtos.UserResponseDto;
+import com.ChatPlateReader.infra.security.TokenService;
+import com.ChatPlateReader.models.User;
 import com.ChatPlateReader.repositories.UserRepository;
 
 @Service
@@ -16,6 +22,16 @@ public class UserService {
 
 	@Autowired
 	UserRepository userRepository;
+
+	@Autowired
+	TokenService tokenService;
+	
+	@Autowired
+	EmailService emailService;
+	
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
 	
 	
 	public UserResponseDto findById(UUID id) {
@@ -28,7 +44,26 @@ public class UserService {
                 .map(user -> new UserResponseDto(user.getUserId(), user.getName(), user.getEmail()))
                 .toList();
 	}
+	public UserResponseDto createUser(UserRequestDto userRequestDto) {
+		if(userRepository.findByEmail(userRequestDto.email()) != null) return null;
+		String encryptedPass = new BCryptPasswordEncoder().encode(userRequestDto.password());
+		
+		var user = new User();
+		BeanUtils.copyProperties(userRequestDto, user);
+		user.setPassword(encryptedPass);
+		emailService.enviarEmailTexto(user.getEmail(), "Conta criada","Bem vindo ao ChatPlateReader " + user.getName() + ", faça bom uso :)");
+		userRepository.save(user);
+		return new UserResponseDto(user.getUserId(), user.getName(), user.getEmail());
+	}
+	public String returnToken(AuthDto authDto) {
+		var usernamePassord = new UsernamePasswordAuthenticationToken(authDto.email(), authDto.password());
+		var auth = this.authenticationManager.authenticate(usernamePassord);
+		var token = tokenService.generateToken((User) auth.getPrincipal());
+		return token;
+	}
+	
 
+	
 	public UserResponseDto updateUser(UUID id, UserRequestDto userRequestDto) {
 		var user = userRepository.findById(id).orElseThrow(()-> new RuntimeException("Cannot be found"));
 		BeanUtils.copyProperties(userRequestDto, user);
